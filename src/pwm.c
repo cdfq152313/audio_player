2f343e0f7afaad5237c38227fcbb5a2b85c51249
Hanty <hpdh0731@hotmail.com>
2014-12-15 14:57:54 +0800

    config PLL clock to 84MHZ and complete pwm for sample rate 42000HZ sin wave

--- src/pwm.c
+++ src/pwm.c
@@ -13,9 +13,9 @@ short wave[42000];
 void PWM_Start(void)
 {
     int i;
-    for(i=0;i<8000;i++){
-        wave[i] = (sin(2.0 * 3.1415926535 * 400.0 * ((double)i / 8000.0) )+1) * 8000 / 2;
-    }
+    for(i=0;i<42000;i++)
+        wave[i] = (sin(2.0 * 3.1415926535 * 400.0 * ((double)i / 42000.0) )+1) * 2000 / 2;
+
     // for(i=0;i<42000;i+= 1000){
     //     fio_printf(1, "%x\r\n", wave[i]);    
     // }
@@ -23,7 +23,7 @@ void PWM_Start(void)
 
     PWM_RCC_Configuration();
     PWM_GPIO_Configuration();
-    PWM_TIM2_Configuration();
+    //PWM_TIM2_Configuration();
     PWM_TIM1_Configuration();
 
     while(1){}
@@ -31,6 +31,24 @@ void PWM_Start(void)
 
 void  PWM_RCC_Configuration(void)
 {
+    ErrorStatus HSEStartUpStatus;
+    RCC_DeInit();
+
+    RCC_HSEConfig(RCC_HSE_ON);
+    HSEStartUpStatus = RCC_WaitForHSEStartUp();
+    if(HSEStartUpStatus == SUCCESS)
+    {
+        RCC_HCLKConfig(RCC_SYSCLK_Div1);
+        RCC_PCLK2Config(RCC_HCLK_Div1);
+        RCC_PCLK1Config(RCC_HCLK_Div2);
+        RCC_PLLConfig(RCC_PLLSource_HSE, 8, 168, 2, 6);
+        RCC_PLLCmd(ENABLE);
+        while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+        while(RCC_GetSYSCLKSource() != 0x08);
+        
+    }
+
     RCC_AHB1PeriphClockCmd( RCC_AHB1Periph_GPIOE , ENABLE );//Enalbe AHB for GPIOE
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_TIM1, ENABLE );//Enable APB for TIM1
     RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM2, ENABLE );//Enable APB for TIM2
@@ -39,34 +57,52 @@ void  PWM_RCC_Configuration(void)
 
 void TIM1_UP_TIM10_IRQHandler(void)
 {
-    play_time_other = TIM_GetCounter(TIM2) % 8000;
-    //TIM1->CCR1 = (sin(2.0 * 3.14159 * 400.0 * ((double)play_time_other / 42000.0) )+1) * 2000 / 2;
-     TIM1->CCR1 = wave[play_time_other];
-    // USART_SendData(USART1, wave[play_time_other]) ;
     TIM_ClearITPendingBit(TIM1, TIM_FLAG_Update);
+    //play_time_other = TIM_GetCounter(TIM2) % 42000;
+    play_time_other = (play_time_other + 1) % 42000;
+    if(play_time_other == 0) play_time_sec++;
+    //TIM1->CCR1 = (sin(2.0 * 3.14159 * 400.0 * ((double)play_time_other / 42000.0) )+1) * 2000 / 2;
+    TIM1->CCR1 = wave[play_time_other];
+    // USART_SendData(USART1, wave[play_time_other]) ;
+    //if(play_time_sec >= 10) TIM1->CCR1 = 0;
 }
 /*
 void TIM2_IRQHandler(void)
 {
-    if( play_time_other >= 42000 ) {
-        play_time_sec++;
-        play_time_other = 0;
-        
-    } else  play_time_other++;
+    TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
 
-    TIM_ClearITPendingBit(TIM2, TIM_FLAG_Update);
-}
-*/
+    //play_time_other = (play_time_other + 1) % 42000;
+    //if(play_time_other == 0) play_time_sec++;
+}*/
+
 void  PWM_TIM2_Configuration(void)
 {
+    NVIC_InitTypeDef NVIC_InitStructure;
     TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
     TIM_OCInitTypeDef  TIM_OCInitStructure;
 
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
   /* Time base configuration */
-    TIM_TimeBaseStructure.TIM_Period = 5250;
+    TIM_TimeBaseStructure.TIM_Period = 1999;
     TIM_TimeBaseStructure.TIM_Prescaler = 0;
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;     
+    TIM_OCInitStructure.TIM_Pulse = 999; 
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC1Init(TIM2, &TIM_OCInitStructure);
+
+    TIM_ARRPreloadConfig(TIM2, ENABLE);
+
+    TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
 
  /* TIM2 enable counter */
     TIM_Cmd(TIM2, ENABLE);
@@ -78,6 +114,8 @@ void  PWM_TIM1_Configuration(void)
     TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
     TIM_OCInitTypeDef  TIM_OCInitStructure;
 
+    //TIM_DeInit(TIM1);
+
     NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_TIM10_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
@@ -85,7 +123,7 @@ void  PWM_TIM1_Configuration(void)
     NVIC_Init(&NVIC_InitStructure);
 
   /* Time base configuration */
-    TIM_TimeBaseStructure.TIM_Period = 10500;
+    TIM_TimeBaseStructure.TIM_Period = 1999;
     TIM_TimeBaseStructure.TIM_Prescaler = 0;
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -94,9 +132,9 @@ void  PWM_TIM1_Configuration(void)
     TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
 
   /* PWM1 Mode configuration: Channel1 */
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 1000;
+    TIM_OCInitStructure.TIM_Pulse = 1999;
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
     TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
 
@@ -121,7 +159,7 @@ void PWM_GPIO_Configuration(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
     GPIO_Init(GPIOE, &GPIO_InitStructure);
 
     GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_TIM1);
