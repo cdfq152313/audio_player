b478ed241710bc5e5b25666f12bb18b11b58f1e3
Hanty <hpdh0731@hotmail.com>
2014-12-17 23:18:56 +0800

    can play 16-bit 42000HZ 2 channel Audio by pwm

--- src/pwm.c
+++ src/pwm.c
@@ -8,20 +8,20 @@
 uint32_t play_time_other = 0;
 uint32_t play_time_sec = 0;
 
-#define BUF_LENGTH 2000
+#define BUF_LENGTH 4000
 
 int i;
 short buf[BUF_LENGTH];
 short buf2[BUF_LENGTH];
 int br;
 int chunck2_size = 0;
-const int btr = BUF_LENGTH * sizeof(uint8_t);
+const int btr = BUF_LENGTH * sizeof(short);
 uint8_t cur_buf = 0;
 uint8_t update_buf = 0;
 uint32_t cur_point = 0;
 FIL fil;
 
-uint8_t back[BUF_LENGTH];
+//uint8_t back[BUF_LENGTH];
 
 //short wave[42000];
 
@@ -33,6 +33,8 @@ void PWM_Start(void)
     // for(i=0;i<42000;i+= 1000){
     //     fio_printf(1, "%x\r\n", wave[i]);    
     // }
+    PWM_RCC_Configuration();
+
     if (f_open(&fil, "music.wav", FA_OPEN_ALWAYS | FA_READ) != FR_OK) {
         fio_printf(1, "open file failed");
         return;
@@ -40,21 +42,34 @@ void PWM_Start(void)
 
     f_lseek(&fil, 40);
     f_read(&fil, &chunck2_size, 4, &br);
-    f_lseek(&fil, f_tell(&fil)+84000);
-    f_read(&fil, buf, btr, &br);
-    f_lseek(&fil, f_tell(&fil)+btr);
-    f_read(&fil, buf2, btr, &br);
-    f_lseek(&fil, f_tell(&fil)+btr);
+    //f_lseek(&fil, 42000+44);
+    //f_read(&fil, buf, btr, &br);
+    //f_lseek(&fil, f_tell(&fil)+btr);
+    //f_read(&fil, buf2, btr, &br);
+    //f_lseek(&fil, f_tell(&fil)+btr);
 
-    int max=40;
+    for(i=0;i<BUF_LENGTH;i++)
+    {
+        buf[i] = 1000;
+        buf2[i] = 1000;
+    }
+
+    int max=2;
     int16_t data;
-    for(i = 0; i < max; i+=2){
-        data = buf2[i*2] | (buf2[i*2+1] << 8);
+
+    for(i = 0; i < max; i+=1){
+        data = buf2[i*8] | (buf2[i*8+1] << 8);
+        fio_printf(1,"%X\r\n", data);
+        data = buf2[i*8+2] | (buf2[i*8+3] << 8);
+        fio_printf(1,"%X\r\n", data);
+        data = buf2[i*8+4] | (buf2[i*8+5] << 8);
+        fio_printf(1,"%X\r\n", data);
+        data = buf2[i*8+6] | (buf2[i*8+7] << 8);
         fio_printf(1,"%X\r\n", data);
     }
 
+
 //fio_printf(1, "%d",chunck2_size);
-    PWM_RCC_Configuration();
     PWM_GPIO_Configuration();
     PWM_TIM2_Configuration();
     PWM_TIM1_Configuration();
@@ -75,13 +90,19 @@ void TIM1_UP_TIM10_IRQHandler(void)
 {
     TIM_ClearITPendingBit(TIM1, TIM_FLAG_Update);
 
-    int32_t data;
-    if(cur_buf == 0) data = buf[cur_point*2] | (buf[cur_point*2+1] << 8);
-    else data = buf2[cur_point*2] | (buf2[cur_point*2+1] << 8);
-
-    TIM1->CCR1 = (data+32768) * 4000 / 65536;
+    if(cur_buf == 0) 
+    {
+        TIM1->CCR1 = buf[cur_point * 2];
+        TIM1->CCR2 = buf[cur_point * 2 + 1];
+    }
+    else
+    {
+        TIM1->CCR1 = buf2[cur_point * 2];
+        TIM1->CCR2 = buf2[cur_point * 2 + 1];
+    }
 
     ++cur_point;
+
     if(cur_point == BUF_LENGTH / 2)  {
         cur_buf = !cur_buf;
         update_buf = 1;
@@ -100,10 +121,30 @@ void TIM2_IRQHandler(void)
     if(update_buf == 1)
     {
         // USART_SendData(USART1, 48) ;
-        if(cur_buf == 0)    f_read(&fil, buf2, btr, &br);
-        else f_read(&fil, buf, btr, &br);
-
-        f_lseek(&fil, f_tell(&fil)+btr);
+        if(cur_buf == 0)   
+        {
+            f_read(&fil, buf2, btr, &br);
+            int i;
+            for(i=0;i<BUF_LENGTH;i++)
+            {
+                //short big = buf2[i*2] & 0x00FF;
+                //short little = (buf2[i*2] & 0xFF00) >> 8;
+                //short data = (big << 8) | little;
+                buf2[i] = (buf2[i]+32768) * 2000 / 65536;
+            }
+        }
+        else
+        {
+            f_read(&fil, buf, btr, &br);
+            int i;
+            for(i=0;i<BUF_LENGTH;i++)
+            {
+                // uint16_t big = buf[i*2] & 0x00FF;
+                // uint16_t little = (buf[i*2] & 0xFF00) >> 8;
+                // short data = (big << 8) | little;
+                buf[i] = (buf[i]+32768) * 2000 / 65536;
+            }
+        }
 
         update_buf = 0;
         // USART_SendData(USART1, 49) ;
@@ -161,7 +202,7 @@ void  PWM_TIM1_Configuration(void)
     NVIC_Init(&NVIC_InitStructure);
 
   /* Time base configuration */
-    TIM_TimeBaseStructure.TIM_Period = 3999;
+    TIM_TimeBaseStructure.TIM_Period = 1999;
     TIM_TimeBaseStructure.TIM_Prescaler = 0;
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -170,15 +211,17 @@ void  PWM_TIM1_Configuration(void)
     TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
 
   /* PWM1 Mode configuration: Channel1 */
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 1999;
+    TIM_OCInitStructure.TIM_Pulse = 999;
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-    TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+    TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
 
     TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+    TIM_OC2Init(TIM1, &TIM_OCInitStructure);
 
     TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+    TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
     TIM_ARRPreloadConfig(TIM1, ENABLE);
 
     TIM_ClearFlag(TIM1, TIM_FLAG_Update);
@@ -201,4 +244,5 @@ void PWM_GPIO_Configuration(void)
     GPIO_Init(GPIOE, &GPIO_InitStructure);
 
     GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_TIM1);
+    GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_TIM1);
 }
